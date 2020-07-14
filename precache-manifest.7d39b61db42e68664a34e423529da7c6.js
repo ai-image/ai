35441a8c337232b0d87f75ea2c8582edecf6d218
nhutthanh340 <nhutthanh340@gmail.com>
2020-07-14 22:43:27 +0700

    Updates

---
+++ precache-manifest.7d39b61db42e68664a34e423529da7c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093afef2c58b0f49154c6950565d978e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34aed69d7d56c823044c",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "4fe7a1f1e883833e61f8",
+    "url": "/static/css/main.e567e731.chunk.css"
+  },
+  {
+    "revision": "34aed69d7d56c823044c",
+    "url": "/static/js/2.4ea07061.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/static/js/2.4ea07061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe7a1f1e883833e61f8",
+    "url": "/static/js/main.ba38332d.chunk.js"
+  },
+  {
+    "revision": "c74d5a4c51fcc6399dcb",
+    "url": "/static/js/runtime-main.80d1f231.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
